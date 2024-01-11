4943f197c09fc47667f5ea1f9ced3da5c2b5d8ed
thort <thort@LAPTOP-27E888GG>
2024-01-11 20:05:15 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SEW_BSP_Kain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben sie eine Ganzzahl ein um zu Prüfen ob sie eine Primzahl ist");
+            int inpt;
+            int.TryParse(Console.ReadLine(), out inpt);
+
+
+            if (inpt % inpt == 0 ) 
+            {
+                Console.WriteLine($"{inpt} ist eine Primzahl");
+            }
+            else { Console.WriteLine($"{inpt} ist keine Primzahl");}
+            Console.ReadKey();
+        }
+    }
+}
